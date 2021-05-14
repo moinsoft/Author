d3c1be16d55856fa183bad5f24835afdc970f263
Mohammad Moin Uddin <66357207+moinsoft@users.noreply.github.com>
2021-05-14 23:47:54 +0600

    Slider Part Added

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  // Slider Activation
+  
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    items: 1,
+    smartSpeed: 1000,
+    nav: true,
+    dots:false
+  })
+
+
+});
