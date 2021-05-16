7174dcf630ad731e4da1882bbfc204caa1b58de4
Mohammad Moin Uddin <66357207+moinsoft@users.noreply.github.com>
2021-05-17 02:12:31 +0600

    Testimonial Part Added

--- js/main.js
+++ js/main.js
@@ -2,12 +2,21 @@ $(document).ready(function () {
 
   // Slider Activation
   
-  $('.owl-carousel').owlCarousel({
+  $('.slider__active').owlCarousel({
     loop: true,
     items: 1,
     smartSpeed: 1000,
     nav: true,
     dots:false
+  })
+
+  //Testimonial Slider Activation
+
+  $('.testimonial__slider__active').owlCarousel({
+    loop: true,
+    items: 1,
+    smartSpeed: 1000,
+    nav: true
   })
 
 
